869fdc2f73ca916054e408a30433041c67523005
Yem Daro <100412991+darororo@users.noreply.github.com>
2025-02-14 18:19:39 +0700

    refactor(main.py): make a function to parse an image into a invoice row
    * Add exception handling when parsing transaction date
    * Log errors when reading an image to a textfile

--- main.py
+++ main.py
@@ -4,46 +4,77 @@ import pytesseract
 import csv
 import helpers
 
-directory = "./data/tests"
-files = Path(directory).glob("*.jpg")
-
-data = []
-fields = ["reference #", "transaction date", "amount-us", "amount-kh", "remark", "seller", "original amount", "from account"]
-
-for file in files:
+def get_row(file: Path, fields: list):
     img = Image.open(file)
     extracted_text = pytesseract.image_to_string(img, "eng+khm")
     lines = extracted_text.splitlines()
-
-    # TODO: turn write to row to a function
     row = {}
-    for idx, line in enumerate(lines):
-        split = line.split(":")
-        if idx == 0:
-            currency = split[0][-3:-1].lower()
-            if currency == "us":
-                row["amount-us"] = split[0][1:-4]
-                row["amount-kh"] = 0
-            elif currency == "kh":
-                row["amount-kh"] = split[0][1:-4]
-                row["amount-us"] = 0
-            continue
+    try:
+        for idx, line in enumerate(lines):
+            split = line.split(":")
+            if idx == 0:
+                currency = split[0][-3:-1].lower()
+                if currency == "us":
+                    row["amount-us"] = split[0][1:-4]
+                    row["amount-kh"] = 0
+                elif currency == "kh":
+                    row["amount-kh"] = split[0][1:-4]
+                    row["amount-us"] = 0
+                continue
 
-        field = split[0].lower()
+            field = split[0].lower()
 
-        if field == "transaction date":
-            date = helpers.string_to_date(split[1][1:-3]) # substr omiting hours and left space
-            split[1] = date
+            if field == "transaction date":
+                try:
+                    time = split[1] + ":" + split[2]
+                    time = time[-8:]
+                    row["time"] = time
+                    
+                    date = helpers.string_to_date(split[1][1:-3]) # substr omiting hours and left space
+                    split[1] = date
+                except:
+                    raise StopIteration
 
-        if len(split) > 1 and (field in fields):
-            row[field] = split[1]
-    data.append(row)
+            if len(split) > 1 and (field in fields):
+                row[field] = split[1]
+    except StopIteration:
+        print("Error parsing date")
+        print("Skipping: " + file.name)
+        raise Exception("error while scanning row")
+    return row
 
-print(data)
-with open('data/tests/output/test.csv', 'w', newline='', encoding="utf-8") as csvfile:
-    writer = csv.DictWriter(csvfile, fieldnames=fields)
-    writer.writeheader()
-    writer.writerows(data)
+def main(): 
+    directory = "data/tests"
+    files = Path(directory).glob("*.jpg")
+
+    fields = ["reference #", "transaction date", "time",  "remark", "amount-us", "amount-kh", "seller", "original amount", "from account"]
+    r = get_row("data/aba-khr-2.jpg", fields)
+    print(r)
+
+    data = []
+    for file in files:
+        print("File: " + file.name)
+        try:
+            row = get_row(file, fields)
+        except Exception as e:
+            print(e)
+            try:
+                with open(directory + "/errors/log.txt", "a", newline="", encoding="utf-8") as errLog:
+                    message = file.name + ": " + str(e)
+                    errLog.write(message + "\n")
+            except Exception as e:
+                print(e)
+        else: 
+            data.append(row)
+
+    print(data)
+    with open(directory + "/output/out2.csv", 'w', newline='', encoding="utf-8") as csvfile:
+        writer = csv.DictWriter(csvfile, fieldnames=fields)
+        writer.writeheader()
+        writer.writerows(data)
+       
+if __name__ == "__main__":
+    main()
 
 
 
